0ca54d615af90f9999c9d19a0a433dd1787edc05
Амирова А.А <alinaamirova01@gmail.com>
2022-11-14 17:22:28 +0500

    5  дз

---
+++ 5 дз/5 дз/Program.cs
@@ -0,0 +1,19 @@
+﻿//Methods
+
+using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static double Count(int a, int b)
+        {
+            return ((Math.Sqrt(a) + a) / b);
+        }
+
+        public static void Main()
+        {
+            Console.WriteLine("Значение выражения равно = " + (Count(6, 2) + Count(13, 5) + Count(21, 3)));
+        }
+    }
+}
