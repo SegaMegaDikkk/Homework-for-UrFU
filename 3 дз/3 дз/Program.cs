aa04d58ab724781f76b7a6d433514a8097aaee06
Амирова А.А <alinaamirova01@gmail.com>
2022-11-14 16:21:51 +0500

    3 дз

---
+++ 3 дз/3 дз/Program.cs
@@ -0,0 +1,19 @@
+﻿//SignsOfNumber
+
+using System;
+
+namespace Nuber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите двухзначное число");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int FirstSign = num / 10;
+            int SecondSign = num % 10;
+            Console.WriteLine("Число десятков - " + FirstSign + " Число единиц - " + SecondSign);
+            Console.WriteLine("Сумма цифр числа - " + FirstSign + SecondSign + " Произведение цифр - " + FirstSign * SecondSign);
+        }
+    }
+}
