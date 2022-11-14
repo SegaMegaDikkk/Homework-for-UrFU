4807e5e6db6e602e23099b37c1485c5803b80544
Амирова А.А <alinaamirova01@gmail.com>
2022-11-14 15:36:47 +0500

    1 дз

---
+++ дз 1/дз 1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Poem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("В. Маяковский ");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("А вы могли бы?");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Я сразу смазал карту будня,\nплеснувши краску из стакана;\nя показал на блюде студня\nкосые скулы океана.\nНа чешуе жестяной рыбы\nпрочел я зовы новых губ.\nА вы\nноктюрн сыграть\nмогли бы\nна флейте водосточных труб?");
+            Console.ResetColor();
+        }
+    }
+}
