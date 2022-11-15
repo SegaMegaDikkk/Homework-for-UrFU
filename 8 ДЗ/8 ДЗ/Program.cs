4fb72f76c6ac1da4238915af5a6ba0f001185de1
Амирова А.А <alinaamirova01@gmail.com>
2022-11-15 19:53:23 +0500

    ЕМАЕ

---
+++ 8 ДЗ/8 ДЗ/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Functions
+
+{
+    class Program
+    {
+        public int FuncUnderZero()
+        {
+            return -1;
+        }
+
+        public int FuncZero()
+        {
+            return 0;
+        }
+
+        public int FuncOverZero()
+        {
+            return 1;
+        }
+        static void Main()
+        {
+            Console.WriteLine("Введите аргумент функции");
+            double x = Convert.ToDouble(Console.ReadLine());
+            var pr = new Program();
+            if (x > 0)
+                Console.WriteLine("Значение функции = " + pr.FuncOverZero());
+            else if (x == 0)
+                Console.WriteLine("Значение функции = " + pr.FuncZero());
+            else 
+                Console.WriteLine("Значение функции = " + pr.FuncUnderZero());
+        }
+    }
+}
