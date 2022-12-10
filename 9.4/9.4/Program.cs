72a28654af7bcdd68081aa89cf2e004809512899
Амирова А.А <alinaamirova01@gmail.com>
2022-12-11 00:28:43 +0500

    9 дз

---
+++ 9.4/9.4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Divide
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите промежуток чисел");
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите искомую сумму делителей");
+            int sumDividersInEachNum = 0;
+            int sumOfDividers = int.Parse(Console.ReadLine());
+            for (int i = firstNum; i <= secondNum; i++)
+            {
+                for (int z = 1; z <= i; z++)
+                {
+                    if (i % z == 0)
+                        sumDividersInEachNum += z;
+                }
+                if (sumDividersInEachNum == sumOfDividers)
+                    Console.WriteLine($"{i} удовлетворяет условию");
+                sumDividersInEachNum = 0;
+
+            }
+        }
+    }
+}
